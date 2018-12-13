cf5d4535bed6e257e9202f7e4a1181c1578153ec
Marian Zburlea <marianzburlea@gmail.com>
2018-12-13 22:40:50 +0000

    step arrows are in

--- src/component/step/step.component.js
+++ src/component/step/step.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './step.scss'
 
 const Step = ({label = 'Step 1', hexColor}) => (
-  <div className="step" style={{backgroundColor: hexColor}}>{label}</div>
+  <div className="step" style={{backgroundColor: hexColor, borderColor: hexColor}}>{label}</div>
 )
 
 export default Step;
